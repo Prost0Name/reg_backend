14d7975d2438917ef7d30682a4c59ad02052fa9b
Prost0Name <danyazazackiy@gmail.com>
2025-02-24 20:54:44 +0500

    url

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,7 +10,7 @@ import (
 func cors(e *echo.Echo) {
 	// CORS middleware
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://87.242.100.33:7000"},
+		AllowOrigins: []string{"https://vsrs-rs.ru"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowMethods: []string{http.MethodGet, http.MethodPost},
 	}))
