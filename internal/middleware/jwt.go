3ccb6bd95cb58a2d862cff2f345e11160b07ffd9
Prost0Name <danyazazackiy@gmail.com>
2025-02-27 20:36:21 +0500

    refact

--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -1,41 +1,41 @@
 package middleware
 
-import (
-	"time"
+// import (
+// 	"time"
 
-	"backend/internal/config"
+// 	"backend/internal/config"
 
-	"github.com/golang-jwt/jwt/v5"
-)
+// 	"github.com/golang-jwt/jwt/v5"
+// )
 
-var jwtKey = []byte(config.JwtSecret)
+// var jwtKey = []byte(config.JwtSecret)
 
-type Claims struct {
-	Login string `json:"login"`
-	jwt.RegisteredClaims
-}
+// type Claims struct {
+// 	Login string `json:"login"`
+// 	jwt.RegisteredClaims
+// }
 
-func GenerateToken(login string) (string, error) {
-	expirationTime := time.Now().Add(24 * time.Hour)
-	claims := &Claims{
-		Login: login,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
+// func GenerateToken(login string) (string, error) {
+// 	expirationTime := time.Now().Add(24 * time.Hour)
+// 	claims := &Claims{
+// 		Login: login,
+// 		RegisteredClaims: jwt.RegisteredClaims{
+// 			ExpiresAt: jwt.NewNumericDate(expirationTime),
+// 		},
+// 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(jwtKey)
-}
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+// 	return token.SignedString(jwtKey)
+// }
 
-func ValidateToken(tokenString string) (*Claims, error) {
-	claims := &Claims{}
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
+// func ValidateToken(tokenString string) (*Claims, error) {
+// 	claims := &Claims{}
+// 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+// 		return jwtKey, nil
+// 	})
 
-	if err != nil || !token.Valid {
-		return nil, err
-	}
-	return claims, nil
-}
+// 	if err != nil || !token.Valid {
+// 		return nil, err
+// 	}
+// 	return claims, nil
+// }
