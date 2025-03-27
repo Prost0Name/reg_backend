40bfb7941de53f09d5403e13efec386116086d98
Prost0Name <danyazazackiy@gmail.com>
2025-03-27 21:55:12 +0500

    register done fixed cors

--- internal/app/middleware/cors.go
+++ internal/app/middleware/cors.go
@@ -9,8 +9,7 @@ import (
 
 func CORS(e *echo.Echo) {
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		// AllowOrigins: []string{"https://vsrs-rs.ru"},
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"https://vsrs-rs.ru"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowMethods: []string{http.MethodGet, http.MethodPost},
 	}))
