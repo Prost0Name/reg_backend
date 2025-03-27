d29bec709cc829d9434a21ac14a6e010d693a29e
Prost0Name <danyazazackiy@gmail.com>
2025-03-27 11:16:33 +0500

    register accept fixed1

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -1,15 +1,16 @@
 package redis
 
 import (
-	"github.com/redis/go-redis/v9"
 	"context"
+
+	"github.com/redis/go-redis/v9"
 )
 
 var Client *redis.Client
 
 func InitRedis() {
 	Client = redis.NewClient(&redis.Options{
-		Addr:     "reg-redis-2zftop:6379",
+		Addr:     "87.242.100.33:6379",
 		Password: "reg2025", // no password set
 		DB:       0,         // use default DB
 	})
@@ -23,4 +24,4 @@ func SetUserData(token string, data interface{}) error {
 func GetUserData(token string) (string, error) {
 	ctx := context.Background()
 	return Client.Get(ctx, token).Result()
-} 
+}
